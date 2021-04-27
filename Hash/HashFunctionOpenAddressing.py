eb915ebf1b417b0449011c6700272995610d752f
eltonsarmanho <s0@19t2&>
2021-04-27 17:21:40 -0300

    Codigo da Estrutura de dados.

--- Hash/HashFunctionOpenAddressing.py
+++ Hash/HashFunctionOpenAddressing.py
@@ -2,8 +2,10 @@ import math
 import numpy as np
 
 class HashingOpenAddressing:
-    def __init__(self,metodo):
+    def __init__(self,metodo,c1=0,c2=1):
         self.metodo = metodo;
+        self.c1 = c1;
+        self.c2 = c2;
 
     def linear(self, chave, hashTable):
         m = len(hashTable[0])
@@ -17,8 +19,23 @@ class HashingOpenAddressing:
     def quadratica(self, chave, hashTable):
         m = len(hashTable[0])
         h1 = chave % m
+        print("Para chave %s" % chave)
         for i in range(m):
-            h = (h1+i*i) %  m
+            h = (h1+self.c1*i+self.c2*i) %  m
+            print("Para i = %s, temos  (h1,h2,h,Rotulo) = (%s,%s,%s) " % (i, h1,h, hashTable[1, h]))
+            if(hashTable[1,h] == 'L'):
+                break;
+        return h;
+
+
+    def duplo(self, chave, hashTable):
+        m = len(hashTable[0])
+        h1 = chave % m
+        h2 = 1 + (chave % (m-1))
+        #print("Para chave %s" % chave)
+        for i in range(m):
+            h = (h1+i*h2) %  m
+            #print("Para i = %s, temos  (h1,h2,h,Rotulo) = (%s,%s,%s,%s) " % (i,h1,h2,h,hashTable[1,h]))
             if(hashTable[1,h] == 'L'):
                 break;
         return h;
@@ -29,6 +46,7 @@ class HashingOpenAddressing:
             endereco = self.linear(valor_chave,hashtable)
         elif(self.metodo == "quadratica"):
             endereco = self.quadratica(valor_chave, hashtable)
+        else: endereco = self.duplo(valor_chave, hashtable)
         hashtable[0, endereco] = valor
         hashtable[1, endereco] = 'O'
     # Mostrar a tabela hash
@@ -38,18 +56,28 @@ class HashingOpenAddressing:
 
 if __name__ == '__main__':
     # Criar uma Hashtable como lista.
+    lst = [10,22,31,4,15,28,17,88,59]
     print("Metodo Tentativa Linear")
-    HashTable = np.array([[None] * 11, ['L'] * 11])
+    HashTable = np.array([[None] * len(lst), ['L'] * len(lst)])
     hashing = HashingOpenAddressing(metodo="linear")
     # Add os elementos na Tabela
-    for valor in [20, 30, 2, 13, 25, 24, 10, 9]:
+    for valor in lst:
         hashing.insere(HashTable, valor, str(valor))
     hashing.display_hash(HashTable)
 
     print("Metodo Tentativa Quadratica")
-    HashTable = np.array([[None] * 11, ['L'] * 11])
-    hashing = HashingOpenAddressing(metodo="quadratica")
+    lst = [10,22,31,4,15,28,17,88,59]
+    HashTable = np.array([[None] * len(lst), ['L'] * len(lst)])
+    hashing = HashingOpenAddressing(metodo="quadratica",c1=1,c2=3)
     # Add os elementos na Tabela
-    for valor in [20, 30, 2, 13, 25, 24, 10, 9]:
+    for valor in lst:
+        hashing.insere(HashTable, valor, str(valor))
+    hashing.display_hash(HashTable)
+
+    print("Metodo Hash Duplo")
+    HashTable = np.array([[None] * len(lst), ['L'] * len(lst)])
+    hashing = HashingOpenAddressing(metodo="duplo")
+    # Add os elementos na Tabela
+    for valor in lst:
         hashing.insere(HashTable, valor, str(valor))
     hashing.display_hash(HashTable)
